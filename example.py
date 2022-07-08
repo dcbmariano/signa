e3b838e3f24a90fbbbaef48c4d70b056318586a2
Diego Mariano <diogohenks@hotmail.com>
2022-07-08 17:29:44 -0300

    Adicionando exemplos

--- example.py
+++ example.py
@@ -4,8 +4,8 @@ import Signa.signa as signa
 #help(signa)  # for obtaining more details use this command
 
 # simple use
-signature = signa.read('./Signa/example/pdb_files/2lzm.pdb', 'csm')
+signature = signa.read('./example/pdb_files/2lzm.pdb', 'csm')
 print(signature)
 
 # Multiple files
-signa.read_csv('./Signa/example/input.csv', signa_type='csm', output='./Signa/example/output.csv')
+signa.read_csv('./example/input.csv', signa_type='csm', output='./example/output.csv')
