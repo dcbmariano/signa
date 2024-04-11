1067c9d6e79ae962d9b49ef3fbca58244b16912b
Diego Mariano <diogohenks@hotmail.com>
2024-04-11 20:50:20 -0300

    Update quickstart.py

--- quickstart.py
+++ quickstart.py
@@ -8,7 +8,7 @@ entry = './docs/examples/as.pdb'
 signature = ''
 
 # Simple use
-signature = signa.read(entry, 'acsm-all', cutoff_limit=12, cutoff_step=0.2, separator=",", cumulative=True)
+#signature = signa.read(entry, 'acsm-all', cutoff_limit=12, cutoff_step=0.2, separator=",", cumulative=True)
 
 # SIGNA-CHARGE
 #signature = signa.read(entry, signa_type='SIGNA-CHARGE', forcefield="AMBER", cutoff_limit=12, cutoff_step=1.0, cumulative=False)
@@ -16,8 +16,8 @@ signature = signa.read(entry, 'acsm-all', cutoff_limit=12, cutoff_step=0.2, sepa
 # signature = signa.read(entry, 'csm', cutoff_limit=30, cutoff_step=0.2, separator=",", cumulative=True)
 
 # Obtaining the labels
-#labels_example = signa.labels(signa_type='csm', cutoff_step=2, cutoff_limit=20, separator=",", cumulative=True)
-#print(labels_example)
+labels_example = signa.labels(signa_type='acsm-all', cutoff_step=0.2, cutoff_limit=20, separator=",", cumulative=True)
+print(labels_example)
 
 # signature = signa.read(entry, 'acsm')
 
@@ -36,6 +36,15 @@ signature = signa.read(entry, 'acsm-all', cutoff_limit=12, cutoff_step=0.2, sepa
 #    #  forcefield='AMBER',
 #  	output='./docs/case_studies/cs4/output_acsm.csv',
 #     cutoff_limit=20
+# )
+# signa.read_csv(
+#  	csv_file='./docs/case_studies/cs6/input.csv', 
+#  	signa_type='acsm-all', 
+#     cumulative=True,
+#    #  forcefield='AMBER',
+#  	output='./docs/case_studies/cs6/output_acsm.csv',
+#     cutoff_limit=20,
+#     cutoff_step=0.2
 # )
 
 # SSV - Comparisons between signatures
